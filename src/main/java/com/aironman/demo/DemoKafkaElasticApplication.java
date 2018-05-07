8d2aea473fd500a7b1019acb751e64b086ec072f
Alonso Isidoro Roman <alonsoir@gmail.com>
2018-05-07 10:59:23 +0200

    first commit.

---
+++ src/main/java/com/aironman/demo/DemoKafkaElasticApplication.java
@@ -0,0 +1,12 @@
+package com.aironman.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKafkaElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKafkaElasticApplication.class, args);
+	}
+}
