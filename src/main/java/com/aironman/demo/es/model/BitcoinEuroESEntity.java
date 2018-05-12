fd35e6900b5db7195dfb0b2d9d2902893217bf9b
Alonso Isidoro Roman <alonsoir@gmail.com>
2018-05-12 20:14:46 +0200

    solving feature-remote-elastic

--- src/main/java/com/aironman/demo/es/model/BitcoinEuroESEntity.java
+++ src/main/java/com/aironman/demo/es/model/BitcoinEuroESEntity.java
@@ -7,232 +7,230 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 
-@Document(indexName = "bitcoin", type = "article")
+@Document(indexName = "bitcoin", type = "article", shards = 1, replicas = 0, refreshInterval = "-1")
 public class BitcoinEuroESEntity {
-
-	@Id
-    private String id;
-    @Field(type = String, index = not_analyzed)
-	private String name;
-    @Field(type = String, index = not_analyzed)
-	private String symbol;
-    @Field(type = String, index = not_analyzed)
-	private String rank;
-    @Field(type = String, index = not_analyzed)
-	private String priceUsd;
-    @Field(type = String, index = not_analyzed)
-	private String priceBtc;
-    @Field(type = String, index = not_analyzed)
-	private String _24hVolumeUsd;
-    @Field(type = String, index = not_analyzed)
-	private String marketCapUsd;
-    @Field(type = String, index = not_analyzed)
-	private String availableSupply;
-    @Field(type = String, index = not_analyzed)
-	private String totalSupply;
-    @Field(type = String, index = not_analyzed)
-	private String maxSupply;
-    @Field(type = String, index = not_analyzed)
-	private String percentChange1h;
-    @Field(type = String, index = not_analyzed)
-	private String percentChange24h;
-    @Field(type = String, index = not_analyzed)
-	private String percentChange7d;
-    @Field(type = String, index = not_analyzed)
-	private String lastUpdated;
-    @Field(type = String, index = not_analyzed)
-	private String priceEur;
-    @Field(type = String, index = not_analyzed)
-	private String _24hVolumeEur;
-    @Field(type = String, index = not_analyzed)
-	private String marketCapEur;
-	
-    public BitcoinEuroESEntity() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public BitcoinEuroESEntity(String id, String name, String symbol, String rank, String priceUsd, String priceBtc,
-			String _24hVolumeUsd, String marketCapUsd, String availableSupply, String totalSupply, String maxSupply,
-			String percentChange1h, String percentChange24h, String percentChange7d, String lastUpdated,
-			String priceEur, String _24hVolumeEur, String marketCapEur) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.symbol = symbol;
-		this.rank = rank;
-		this.priceUsd = priceUsd;
-		this.priceBtc = priceBtc;
-		this._24hVolumeUsd = _24hVolumeUsd;
-		this.marketCapUsd = marketCapUsd;
-		this.availableSupply = availableSupply;
-		this.totalSupply = totalSupply;
-		this.maxSupply = maxSupply;
-		this.percentChange1h = percentChange1h;
-		this.percentChange24h = percentChange24h;
-		this.percentChange7d = percentChange7d;
-		this.lastUpdated = lastUpdated;
-		this.priceEur = priceEur;
-		this._24hVolumeEur = _24hVolumeEur;
-		this.marketCapEur = marketCapEur;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSymbol() {
-		return symbol;
-	}
-
-	public void setSymbol(String symbol) {
-		this.symbol = symbol;
-	}
-
-	public String getRank() {
-		return rank;
-	}
-
-	public void setRank(String rank) {
-		this.rank = rank;
-	}
-
-	public String getPriceUsd() {
-		return priceUsd;
-	}
-
-	public void setPriceUsd(String priceUsd) {
-		this.priceUsd = priceUsd;
-	}
-
-	public String getPriceBtc() {
-		return priceBtc;
-	}
-
-	public void setPriceBtc(String priceBtc) {
-		this.priceBtc = priceBtc;
-	}
-
-	public String get_24hVolumeUsd() {
-		return _24hVolumeUsd;
-	}
-
-	public void set_24hVolumeUsd(String _24hVolumeUsd) {
-		this._24hVolumeUsd = _24hVolumeUsd;
-	}
-
-	public String getMarketCapUsd() {
-		return marketCapUsd;
-	}
-
-	public void setMarketCapUsd(String marketCapUsd) {
-		this.marketCapUsd = marketCapUsd;
-	}
-
-	public String getAvailableSupply() {
-		return availableSupply;
-	}
-
-	public void setAvailableSupply(String availableSupply) {
-		this.availableSupply = availableSupply;
-	}
-
-	public String getTotalSupply() {
-		return totalSupply;
-	}
-
-	public void setTotalSupply(String totalSupply) {
-		this.totalSupply = totalSupply;
-	}
-
-	public String getMaxSupply() {
-		return maxSupply;
-	}
-
-	public void setMaxSupply(String maxSupply) {
-		this.maxSupply = maxSupply;
-	}
-
-	public String getPercentChange1h() {
-		return percentChange1h;
-	}
-
-	public void setPercentChange1h(String percentChange1h) {
-		this.percentChange1h = percentChange1h;
-	}
-
-	public String getPercentChange24h() {
-		return percentChange24h;
-	}
-
-	public void setPercentChange24h(String percentChange24h) {
-		this.percentChange24h = percentChange24h;
-	}
-
-	public String getPercentChange7d() {
-		return percentChange7d;
-	}
-
-	public void setPercentChange7d(String percentChange7d) {
-		this.percentChange7d = percentChange7d;
-	}
-
-	public String getLastUpdated() {
-		return lastUpdated;
-	}
-
-	public void setLastUpdated(String lastUpdated) {
-		this.lastUpdated = lastUpdated;
-	}
-
-	public String getPriceEur() {
-		return priceEur;
-	}
-
-	public void setPriceEur(String priceEur) {
-		this.priceEur = priceEur;
-	}
-
-	public String get_24hVolumeEur() {
-		return _24hVolumeEur;
-	}
-
-	public void set_24hVolumeEur(String _24hVolumeEur) {
-		this._24hVolumeEur = _24hVolumeEur;
-	}
-
-	public String getMarketCapEur() {
-		return marketCapEur;
-	}
-
-	public void setMarketCapEur(String marketCapEur) {
-		this.marketCapEur = marketCapEur;
-	}
-
-	@Override
-	public String toString() {
-		return "BitcoinEuroESEntity [id=" + id + ", name=" + name + ", symbol=" + symbol + ", rank=" + rank
-				+ ", priceUsd=" + priceUsd + ", priceBtc=" + priceBtc + ", _24hVolumeUsd=" + _24hVolumeUsd
-				+ ", marketCapUsd=" + marketCapUsd + ", availableSupply=" + availableSupply + ", totalSupply="
-				+ totalSupply + ", maxSupply=" + maxSupply + ", percentChange1h=" + percentChange1h
-				+ ", percentChange24h=" + percentChange24h + ", percentChange7d=" + percentChange7d + ", lastUpdated="
-				+ lastUpdated + ", priceEur=" + priceEur + ", _24hVolumeEur=" + _24hVolumeEur + ", marketCapEur="
-				+ marketCapEur + "]";
-	}
-
     
-	
-	
+    @Id
+    private String id;
+    // index can be not_analyzed and analyzed
+    @Field(type = String, index = not_analyzed, store = true)
+    private String name;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String symbol;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String rank;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String priceUsd;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String priceBtc;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String _24hVolumeUsd;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String marketCapUsd;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String availableSupply;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String totalSupply;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String maxSupply;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String percentChange1h;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String percentChange24h;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String percentChange7d;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String lastUpdated;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String priceEur;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String _24hVolumeEur;
+    @Field(type = String, index = not_analyzed, store = true)
+    private String marketCapEur;
+    
+    public BitcoinEuroESEntity() {
+	super();
+	// TODO Auto-generated constructor stub
+    }
+    
+    public BitcoinEuroESEntity(String id, String name, String symbol, String rank, String priceUsd, String priceBtc,
+            String _24hVolumeUsd, String marketCapUsd, String availableSupply, String totalSupply, String maxSupply,
+            String percentChange1h, String percentChange24h, String percentChange7d, String lastUpdated,
+            String priceEur, String _24hVolumeEur, String marketCapEur) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.symbol = symbol;
+	this.rank = rank;
+	this.priceUsd = priceUsd;
+	this.priceBtc = priceBtc;
+	this._24hVolumeUsd = _24hVolumeUsd;
+	this.marketCapUsd = marketCapUsd;
+	this.availableSupply = availableSupply;
+	this.totalSupply = totalSupply;
+	this.maxSupply = maxSupply;
+	this.percentChange1h = percentChange1h;
+	this.percentChange24h = percentChange24h;
+	this.percentChange7d = percentChange7d;
+	this.lastUpdated = lastUpdated;
+	this.priceEur = priceEur;
+	this._24hVolumeEur = _24hVolumeEur;
+	this.marketCapEur = marketCapEur;
+    }
+    
+    public String getId() {
+	return id;
+    }
+    
+    public void setId(String id) {
+	this.id = id;
+    }
+    
+    public String getName() {
+	return name;
+    }
+    
+    public void setName(String name) {
+	this.name = name;
+    }
+    
+    public String getSymbol() {
+	return symbol;
+    }
+    
+    public void setSymbol(String symbol) {
+	this.symbol = symbol;
+    }
+    
+    public String getRank() {
+	return rank;
+    }
+    
+    public void setRank(String rank) {
+	this.rank = rank;
+    }
+    
+    public String getPriceUsd() {
+	return priceUsd;
+    }
+    
+    public void setPriceUsd(String priceUsd) {
+	this.priceUsd = priceUsd;
+    }
+    
+    public String getPriceBtc() {
+	return priceBtc;
+    }
+    
+    public void setPriceBtc(String priceBtc) {
+	this.priceBtc = priceBtc;
+    }
+    
+    public String get_24hVolumeUsd() {
+	return _24hVolumeUsd;
+    }
+    
+    public void set_24hVolumeUsd(String _24hVolumeUsd) {
+	this._24hVolumeUsd = _24hVolumeUsd;
+    }
+    
+    public String getMarketCapUsd() {
+	return marketCapUsd;
+    }
+    
+    public void setMarketCapUsd(String marketCapUsd) {
+	this.marketCapUsd = marketCapUsd;
+    }
+    
+    public String getAvailableSupply() {
+	return availableSupply;
+    }
+    
+    public void setAvailableSupply(String availableSupply) {
+	this.availableSupply = availableSupply;
+    }
+    
+    public String getTotalSupply() {
+	return totalSupply;
+    }
+    
+    public void setTotalSupply(String totalSupply) {
+	this.totalSupply = totalSupply;
+    }
+    
+    public String getMaxSupply() {
+	return maxSupply;
+    }
+    
+    public void setMaxSupply(String maxSupply) {
+	this.maxSupply = maxSupply;
+    }
+    
+    public String getPercentChange1h() {
+	return percentChange1h;
+    }
+    
+    public void setPercentChange1h(String percentChange1h) {
+	this.percentChange1h = percentChange1h;
+    }
+    
+    public String getPercentChange24h() {
+	return percentChange24h;
+    }
+    
+    public void setPercentChange24h(String percentChange24h) {
+	this.percentChange24h = percentChange24h;
+    }
+    
+    public String getPercentChange7d() {
+	return percentChange7d;
+    }
+    
+    public void setPercentChange7d(String percentChange7d) {
+	this.percentChange7d = percentChange7d;
+    }
+    
+    public String getLastUpdated() {
+	return lastUpdated;
+    }
+    
+    public void setLastUpdated(String lastUpdated) {
+	this.lastUpdated = lastUpdated;
+    }
+    
+    public String getPriceEur() {
+	return priceEur;
+    }
+    
+    public void setPriceEur(String priceEur) {
+	this.priceEur = priceEur;
+    }
+    
+    public String get_24hVolumeEur() {
+	return _24hVolumeEur;
+    }
+    
+    public void set_24hVolumeEur(String _24hVolumeEur) {
+	this._24hVolumeEur = _24hVolumeEur;
+    }
+    
+    public String getMarketCapEur() {
+	return marketCapEur;
+    }
+    
+    public void setMarketCapEur(String marketCapEur) {
+	this.marketCapEur = marketCapEur;
+    }
+    
+    @Override
+    public String toString() {
+	return "BitcoinEuroESEntity [id=" + id + ", name=" + name + ", symbol=" + symbol + ", rank=" + rank
+	        + ", priceUsd=" + priceUsd + ", priceBtc=" + priceBtc + ", _24hVolumeUsd=" + _24hVolumeUsd
+	        + ", marketCapUsd=" + marketCapUsd + ", availableSupply=" + availableSupply + ", totalSupply="
+	        + totalSupply + ", maxSupply=" + maxSupply + ", percentChange1h=" + percentChange1h
+	        + ", percentChange24h=" + percentChange24h + ", percentChange7d=" + percentChange7d + ", lastUpdated="
+	        + lastUpdated + ", priceEur=" + priceEur + ", _24hVolumeEur=" + _24hVolumeEur + ", marketCapEur="
+	        + marketCapEur + "]";
+    }
+    
 }
--- src/main/java/com/aironman/demo/kafka/MessageListener.java
+++ src/main/java/com/aironman/demo/kafka/MessageListener.java
@@ -15,103 +15,104 @@ import com.aironman.demo.es.model.BitcoinEuroESEntity;
 import com.aironman.demo.es.service.BitCoinESService;
 
 /***
- * Naive Kafka consumer implementation. I must interwire here the elastic search connector.
+ * Naive Kafka consumer implementation. I must interwire here the elastic search
+ * connector.
+ * 
  * @author aironman
  *
  */
 public class MessageListener {
-
-	Logger logger = LoggerFactory.getLogger(getClass());
-
-	@Autowired
-	BitCoinESService bcESService;
-	// TODO what it is means the number?
+    
+    Logger logger = LoggerFactory.getLogger(getClass());
+    
+    @Autowired
+    BitCoinESService bcESService;
+    // TODO what it is means the number?
     private CountDownLatch latch = new CountDownLatch(3);
-
+    
     private CountDownLatch partitionLatch = new CountDownLatch(2);
-
+    
     private CountDownLatch filterLatch = new CountDownLatch(2);
-
+    
     private CountDownLatch greetingLatch = new CountDownLatch(1);
     
     private CountDownLatch bitCoinLatch = new CountDownLatch(1);
-
+    
     @KafkaListener(topics = "${greeting.topic.name}", group = "foo", containerFactory = "fooKafkaListenerContainerFactory")
     public void listenGroupFoo(String message) {
-    	logger.info("Received Messasge in group 'foo': " + message);
-        latch.countDown();
+	logger.info("Received Messasge in group 'foo': " + message);
+	latch.countDown();
     }
-
+    
     @KafkaListener(topics = "${greeting.topic.name}", group = "bar", containerFactory = "barKafkaListenerContainerFactory")
     public void listenGroupBar(String message) {
-    	logger.info("Received Messasge in group 'bar': " + message);
-        latch.countDown();
+	logger.info("Received Messasge in group 'bar': " + message);
+	latch.countDown();
     }
-
+    
     @KafkaListener(topics = "${greeting.topic.name}", containerFactory = "headersKafkaListenerContainerFactory")
     public void listenWithHeaders(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
-        logger.info("Received Messasge: " + message + " from partition: " + partition);
-        latch.countDown();
+	logger.info("Received Messasge: " + message + " from partition: " + partition);
+	latch.countDown();
     }
-
+    
     @KafkaListener(topicPartitions = @TopicPartition(topic = "${partitioned.topic.name}", partitions = { "0", "3" }))
     public void listenToParition(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
-        logger.info("Received Message: " + message + " from partition: " + partition);
-        this.partitionLatch.countDown();
+	logger.info("Received Message: " + message + " from partition: " + partition);
+	this.partitionLatch.countDown();
     }
-
+    
     @KafkaListener(topics = "${filtered.topic.name}", containerFactory = "filterKafkaListenerContainerFactory")
     public void listenWithFilter(String message) {
-        logger.info("Recieved Message in filtered listener: " + message);
-        this.filterLatch.countDown();
+	logger.info("Recieved Message in filtered listener: " + message);
+	this.filterLatch.countDown();
     }
-
+    
     @KafkaListener(topics = "${greeting.topic.name}", containerFactory = "greetingKafkaListenerContainerFactory")
     public void greetingListener(Greeting greeting) {
-        logger.info("Recieved greeting message: " + greeting);
-        this.greetingLatch.countDown();
+	logger.info("Recieved greeting message: " + greeting);
+	this.greetingLatch.countDown();
     }
     
     /***
-     * This method is invoked when a new message arrives to the topic. 
-     * @param bitcoinEuroKafkaEntity the new message from the topic.
+     * This method is invoked when a new message arrives to the topic.
+     * 
+     * @param bitcoinEuroKafkaEntity
+     *            the new message from the topic.
      */
     @KafkaListener(topics = "${message.topic.name}", containerFactory = "bitCoinKafkaListenerContainerFactory")
     public void bitCoinListener(BitcoinEuroKafkaEntity bitcoinEuroKafkaEntity) {
-    	logger.info("Recieved bitcoinEuroKafkaEntity message: " + bitcoinEuroKafkaEntity);
-    	BitcoinEuroESEntity entity = createElasticPojoFromKafkaPojo(bitcoinEuroKafkaEntity);
-    	BitcoinEuroESEntity saved = bcESService.save(entity );
-		logger.info("Entity saved in ES. " +saved .toString() );
-    	this.bitCoinLatch.countDown();
+	logger.info("kafka message: " + bitcoinEuroKafkaEntity);
+	BitcoinEuroESEntity entity = createElasticPojoFromKafkaPojo(bitcoinEuroKafkaEntity);
+	BitcoinEuroESEntity saved = bcESService.save(entity);
+	logger.info("ES Entity: " + saved.toString());
+	this.bitCoinLatch.countDown();
     }
-
-	private BitcoinEuroESEntity createElasticPojoFromKafkaPojo(BitcoinEuroKafkaEntity bitcoinEuroKafkaEntity) {
-		BitcoinEuroESEntity entity = new BitcoinEuroESEntity();
-    	entity .set_24hVolumeEur(bitcoinEuroKafkaEntity.get_24hVolumeEur());
-    	entity.set_24hVolumeUsd(bitcoinEuroKafkaEntity.get_24hVolumeUsd());
-    	entity.setAvailableSupply(bitcoinEuroKafkaEntity.getAvailableSupply());
-    	entity.setId(bitcoinEuroKafkaEntity.getId());
-    	entity.setLastUpdated(bitcoinEuroKafkaEntity.getLastUpdated());
-    	entity.setMarketCapEur(bitcoinEuroKafkaEntity.getMarketCapEur());
-    	entity.setMarketCapUsd(bitcoinEuroKafkaEntity.getMarketCapUsd());
-    	entity.setMaxSupply(bitcoinEuroKafkaEntity.getMaxSupply());
-    	entity.setName(bitcoinEuroKafkaEntity.getName());
-    	entity.setPercentChange1h(bitcoinEuroKafkaEntity.getPercentChange1h());
-    	entity.setPriceEur(bitcoinEuroKafkaEntity.getPriceEur());
-    	entity.setPriceBtc(bitcoinEuroKafkaEntity.getPriceBtc());
-    	entity.setPercentChange24h(bitcoinEuroKafkaEntity.getPercentChange24h());
-    	entity.setPercentChange7d(bitcoinEuroKafkaEntity.getPercentChange7d());
-    	entity.setRank(bitcoinEuroKafkaEntity.getRank());
-    	entity.setSymbol(bitcoinEuroKafkaEntity.getSymbol());
-    	entity.setTotalSupply(bitcoinEuroKafkaEntity.getTotalSupply());
-		return entity;
-	}
-
-	public CountDownLatch getBitCoinLatch() {
-		return bitCoinLatch;
-	}
-
-	
+    
+    private BitcoinEuroESEntity createElasticPojoFromKafkaPojo(BitcoinEuroKafkaEntity bitcoinEuroKafkaEntity) {
+	BitcoinEuroESEntity entity = new BitcoinEuroESEntity();
+	entity.set_24hVolumeEur(bitcoinEuroKafkaEntity.get_24hVolumeEur());
+	entity.set_24hVolumeUsd(bitcoinEuroKafkaEntity.get_24hVolumeUsd());
+	entity.setAvailableSupply(bitcoinEuroKafkaEntity.getAvailableSupply());
+	entity.setId(bitcoinEuroKafkaEntity.getId());
+	entity.setLastUpdated(bitcoinEuroKafkaEntity.getLastUpdated());
+	entity.setMarketCapEur(bitcoinEuroKafkaEntity.getMarketCapEur());
+	entity.setMarketCapUsd(bitcoinEuroKafkaEntity.getMarketCapUsd());
+	entity.setMaxSupply(bitcoinEuroKafkaEntity.getMaxSupply());
+	entity.setName(bitcoinEuroKafkaEntity.getName());
+	entity.setPercentChange1h(bitcoinEuroKafkaEntity.getPercentChange1h());
+	entity.setPriceEur(bitcoinEuroKafkaEntity.getPriceEur());
+	entity.setPriceBtc(bitcoinEuroKafkaEntity.getPriceBtc());
+	entity.setPercentChange24h(bitcoinEuroKafkaEntity.getPercentChange24h());
+	entity.setPercentChange7d(bitcoinEuroKafkaEntity.getPercentChange7d());
+	entity.setRank(bitcoinEuroKafkaEntity.getRank());
+	entity.setSymbol(bitcoinEuroKafkaEntity.getSymbol());
+	entity.setTotalSupply(bitcoinEuroKafkaEntity.getTotalSupply());
+	return entity;
+    }
+    
+    public CountDownLatch getBitCoinLatch() {
+	return bitCoinLatch;
+    }
     
 }
-
