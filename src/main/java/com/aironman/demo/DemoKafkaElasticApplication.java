a52a201654fd8fb5a83c5c3d96275b1bd7314401
Alonso Isidoro Roman <alonsoir@gmail.com>
2018-05-16 16:42:28 +0200

    adding eureka discovery client. ATTENTION! Test is not working! need more work.

--- src/main/java/com/aironman/demo/DemoKafkaElasticApplication.java
+++ src/main/java/com/aironman/demo/DemoKafkaElasticApplication.java
@@ -12,12 +12,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @PropertySource(value = { "application.properties" })
+@EnableEurekaClient
+@RestController
 public class DemoKafkaElasticApplication {
     
     Logger logger = LoggerFactory.getLogger(getClass());
@@ -51,13 +56,19 @@ public class DemoKafkaElasticApplication {
 	    Map<String, String> mapSettings = settings.getAsMap();
 	    Set<String> keySet = mapSettings.keySet();
 	    for (String key : keySet)
-		logger.info("key: " + key + " value: " + mapSettings.get(key));
+		logger.info("key: " + key +
+		        " value: " + mapSettings.get(key));
 	    
 	} catch (Exception e) {
 	    logger.error("ups!" + e.getMessage());
-	    
+	    System.exit(-1);
 	}
 	logger.info("END @PostConstruct...");
 	
+    }
+    
+    @RequestMapping("/greeting")
+    public String greeting() {
+	return "Hello from EurekaClient! This is DemoKafkaElasticApplication...";
     }
 }
